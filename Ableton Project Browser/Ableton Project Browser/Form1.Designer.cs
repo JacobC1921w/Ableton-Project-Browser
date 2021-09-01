3077a834ead3a275226fd2008ea66e319221d1bb
Jacob Collins <jacobc1926@gmail.com>
2021-09-01 13:34:41 +1000

    Initial commit

---
+++ Ableton Project Browser/Ableton Project Browser/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Ableton_Project_Browser {
+    partial class MainForm {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mainDataGrid = new System.Windows.Forms.DataGridView();
+            this.nameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.keyColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BPMColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.locationColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 244);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(8, 8);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // mainDataGrid
+            // 
+            this.mainDataGrid.AllowUserToAddRows = false;
+            this.mainDataGrid.AllowUserToDeleteRows = false;
+            this.mainDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.mainDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.mainDataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameColumn,
+            this.keyColumn,
+            this.BPMColumn,
+            this.descriptionColumn,
+            this.locationColumn});
+            this.mainDataGrid.Location = new System.Drawing.Point(19, 13);
+            this.mainDataGrid.Name = "mainDataGrid";
+            this.mainDataGrid.RowTemplate.Height = 25;
+            this.mainDataGrid.Size = new System.Drawing.Size(769, 425);
+            this.mainDataGrid.TabIndex = 1;
+            this.mainDataGrid.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.mainDataGrid_CellEndEdit);
+            this.mainDataGrid.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.mainDataGrid_RowHeaderMouseDoubleClick);
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.HeaderText = "Name";
+            this.nameColumn.Name = "nameColumn";
+            // 
+            // keyColumn
+            // 
+            this.keyColumn.HeaderText = "Key";
+            this.keyColumn.Name = "keyColumn";
+            // 
+            // BPMColumn
+            // 
+            this.BPMColumn.HeaderText = "BPM";
+            this.BPMColumn.Name = "BPMColumn";
+            // 
+            // descriptionColumn
+            // 
+            this.descriptionColumn.HeaderText = "Description";
+            this.descriptionColumn.Name = "descriptionColumn";
+            // 
+            // locationColumn
+            // 
+            this.locationColumn.HeaderText = "Location";
+            this.locationColumn.Name = "locationColumn";
+            this.locationColumn.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mainDataGrid);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "Ableton Project Browser (double click to change default project path)";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DoubleClick += new System.EventHandler(this.MainForm_DoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainDataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView mainDataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn keyColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BPMColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn locationColumn;
+    }
+}
+
---
+++ Ableton Project Browser/Ableton Project Browser/Form1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.IO;
+using System.Linq;
+using System.IO.Compression;
+using System.Diagnostics;
+
+namespace Ableton_Project_Browser {
+    public partial class MainForm : Form {
+
+        /// <summary>
+        ///  Make defaultProjectLocation a global variable
+        /// </summary>
+        public string defaultProjectLocation { get; private set; }
+        public MainForm() => InitializeComponent();
+
+        /// <summary>
+        ///   A method for opening the gzip .als project file, "parsing" the XML and finding the song temp. By far the worst piece of code on my whole repository
+        /// </summary>
+        /// <param name="file">The full location of the .als file.</param>
+        string getTempoFromALSFile(string file) {
+            using (FileStream fileReader = File.OpenRead(file)) {
+                using(GZipStream gzip = new(fileReader, CompressionMode.Decompress, true)) {
+                    using(StreamReader ungzip = new(gzip)) {
+                        string extractedText = ungzip.ReadToEnd();
+                        using (StringReader stringReader = new(extractedText.Substring(extractedText.IndexOf("<Tempo>") + "<Tempo>".Length, extractedText.LastIndexOf("</Tempo>") - extractedText.IndexOf("<Tempo>") + "<Tempo>".Length))) {
+                            string line = string.Empty;
+                            do {
+                                line = stringReader.ReadLine();
+                                if(line != null) {
+                                    if(line.Contains("<Manual Value=\"")) {
+                                        return line.Split("\"")[1];
+                                    }
+                                }
+                            } while(line != null);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e) {
+            ///
+            /// Disable the name and BPM columns, I don't want you to edit this :p
+            ///
+            mainDataGrid.Columns["nameColumn"].ReadOnly = true;
+            mainDataGrid.Columns["BPMColumn"].ReadOnly = true;
+
+            ///
+            /// Registry checking stuff
+            ///
+            if (Registry.CurrentUser.OpenSubKey(@"SOFTWARE\APB") == null) {
+                Registry.CurrentUser.CreateSubKey(@"SOFTWARE\APB");
+            }
+
+            ///
+            /// Create a sub-key for the default project directory
+            ///
+            if (Registry.CurrentUser.OpenSubKey(@"SOFTWARE\APB").GetValue("defaultProjectPath") == null) {
+                Registry.CurrentUser.CreateSubKey(@"SOFTWARE\APB").SetValue("defaultProjectPath", Interaction.InputBox("Default Project Location", "Default Project Location"));
+            }
+
+
+            ///
+            /// Iterate over projects in the path, making sure its a valid project directory
+            ///
+            foreach (string directory in Directory.EnumerateDirectories(Registry.CurrentUser.OpenSubKey(@"SOFTWARE\APB").GetValue("defaultProjectPath").ToString())) {
+                if(Directory.GetFiles(directory, "*.als", SearchOption.TopDirectoryOnly).Length > 0) {
+                    ///
+                    /// Create a config file if it doesn't exist already
+                    ///
+                    if (!File.Exists(directory + @"\APB.yaml")) {
+                        File.WriteAllLines(directory + @"\APB.yaml", new string[] { "Name: " + directory.Split(@"\").Last().Replace(" Project", ""), "Key: ", "BPM: " + getTempoFromALSFile(directory + @"\" + directory.Split(@"\").Last().Replace(" Project", "") + ".als"), "Description: " });
+                    }
+
+
+                    ///
+                    /// Start setting up the datagridview
+                    ///
+                    int index = mainDataGrid.Rows.Add();
+
+                    mainDataGrid.Rows[index].Cells["nameColumn"].Value = "";
+                    mainDataGrid.Rows[index].Cells["keyColumn"].Value = "";
+                    mainDataGrid.Rows[index].Cells["BPMColumn"].Value = "";
+                    mainDataGrid.Rows[index].Cells["descriptionColumn"].Value = "";
+                    mainDataGrid.Rows[index].Cells["locationColumn"].Value = directory;
+
+                    foreach (string line in File.ReadAllLines(directory + @"\APB.yaml")) {
+                        if(line.StartsWith("Name: ")) {
+                            mainDataGrid.Rows[index].Cells["nameColumn"].Value = line[6..];
+                        } else if (line.StartsWith("Key: ")) {
+                            mainDataGrid.Rows[index].Cells["keyColumn"].Value = line[5..];
+                        } else if (line.StartsWith("BPM: ")) {
+                            mainDataGrid.Rows[index].Cells["BPMColumn"].Value = line[5..];
+                        } else if (line.StartsWith("Description: ")) {
+                            mainDataGrid.Rows[index].Cells["descriptionColumn"].Value = line[13..];
+                        }
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// When doubleclicking the main form, allow the default project location to be changed. TODO: refresh main form
+        /// </summary>
+        private void MainForm_DoubleClick(object sender, EventArgs e) {
+            Registry.CurrentUser.CreateSubKey(@"SOFTWARE\APB").SetValue("defaultProjectPath", Interaction.InputBox("Default Project Location", "Default Project Location", Registry.CurrentUser.OpenSubKey(@"SOFTWARE\APB").GetValue("defaultProjectPath").ToString()));
+            defaultProjectLocation = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\APB").GetValue("defaultProjectPath").ToString();
+        }
+
+        /// <summary>
+        /// Update project config file based on edited data
+        /// </summary>
+        private void mainDataGrid_CellEndEdit(object sender, DataGridViewCellEventArgs e) {
+            File.WriteAllLines(mainDataGrid.Rows[e.RowIndex].Cells["locationColumn"].Value + @"\APB.yaml", new string[] { "Name: " + mainDataGrid.Rows[e.RowIndex].Cells["nameColumn"].Value, "Key: " + mainDataGrid.Rows[e.RowIndex].Cells["keyColumn"].Value, "BPM: " + mainDataGrid.Rows[e.RowIndex].Cells["BPMColumn"].Value, "Description: " + mainDataGrid.Rows[e.RowIndex].Cells["descriptionColumn"].Value });
+        }
+
+        /// <summary>
+        /// Make row headers doubleclickable, and open ableton with the project.
+        /// </summary>
+        private void mainDataGrid_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e) {
+            Process proc = new();
+            proc.StartInfo.FileName = Registry.ClassesRoot.OpenSubKey(@"Ableton.Live.AppLiveSuite.als.10\shell\Open\Command").GetValue("").ToString().Replace("\"%1\"", "");
+            proc.StartInfo.Arguments = "\"" + Registry.CurrentUser.OpenSubKey(@"SOFTWARE\APB").GetValue("defaultProjectPath").ToString() + @"\" + mainDataGrid.Rows[e.RowIndex].Cells["nameColumn"].Value + @" Project\" + mainDataGrid.Rows[e.RowIndex].Cells["nameColumn"].Value + ".als\"";
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = true;
+            proc.Start();
+        }
+    }
+}
